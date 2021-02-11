5acadfdd4fc6f9380bccbab5f860b4fbfe1b1732
JordanUkawoko <77895913+JordanUkawoko@users.noreply.github.com>
2021-02-11 11:27:09 +0000

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("Welcome to Python, what is your name?")
+name = input()
+print("It is very nice to meet you, " + name)
+print("So... " + name, " What is your age?")
+age = int(input())
+print("Oh your an old one")
+print("You will be " + str(int(age) + 1), " in one year!!")
