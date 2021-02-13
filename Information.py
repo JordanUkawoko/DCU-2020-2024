283a80af5d287fc40588e482497814e9e14fb601
JordanUkawoko <77895913+JordanUkawoko@users.noreply.github.com>
2021-02-13 10:41:12 +0000

    Create Information.py

---
+++ Information.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+while True:
+    print("please enter your location")
+    location = input()
+    break
+print("Thanks for entering your location")
+
+
+while True:
+    print("please enter your name")
+    name = input()
+    break
+print("Thanks for entering your name")
+
+
+while True:
+    print("please enter your age")
+    age = input()
+    break
+print("Thanks for entering your age")
+
+print("so your from " + location)
+print("and your name is " + name)
+print("and you are " + age)
+print("Thanks for entering your info " + name)
